4d3b8493e46d7fd168fb392694a98efbbaee4e9e
Tselmeg Otgonbayar <z5594044@ad.unsw.edu.au>
2026-05-26 10:18:19 +1000

    feat: the bundler is working and is configured correctly so that the chart is working on the website

--- website/chart_setup.js
+++ website/chart_setup.js
@@ -3,9 +3,15 @@ import {
     createChart
 } from 'lightweight-charts'
 
+const chartOptions = {
+    crosshair: {
+        mode: 0
+    }
+}
+
 const chartRectangle = document.querySelector('.main-loop .chart-rectangle')
 
-const chart = createChart(chartRectangle)
+const chart = createChart(chartRectangle, chartOptions)
 
 const candleStickSeries = chart.addSeries(CandlestickSeries, {
     upColor: '#26a69a',
@@ -15,7 +21,7 @@ const candleStickSeries = chart.addSeries(CandlestickSeries, {
     wickDownColor: '#ef5350'
 })
 
-candlestickSeries.setData([
+candleStickSeries.setData([
     { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
     { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
     { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },

d5feaa6e86163742508767f0512f839be39d8d46
Tselmeg Otgonbayar <tselmeg.rim@gmail.com>
2026-05-26 16:48:34 +1000

    added the api URL with its options

--- get_chart_data.js
+++ get_chart_data.js
@@ -7,7 +7,19 @@ import {
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const dataAPIUrl = ''
+const dataAPIUrl = 'https://api.twelvedata.com/time_series?symbol=XAU/USD&start_date=2026-3-1&end_date=2026-3-9&interval=15min&apikey='
+const symbol = 'XAU/USD'
+const startingDate = '2026-3-1'
+const endingDate = '2026-3-9'
+const interval = '15min'
+const apiKey = process.env.TWELVEDATA_MY_API_KEY
+
+const dataAPIUrl = `https://api.twelvedata.com/time_series?
+                    symbol=${symbol}&
+                    start_date=${startingDate}&
+                    end_date=${endingDate}&
+                    interval=${interval}&
+                    apikey=${apiKey}`
 
 async function getDataFromAPIAndSetFile() {
     const response = await fetch(dataAPIUrl)

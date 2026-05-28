4d8711e528218fff4043191237b6f33ec587424b
Tselmeg Otgonbayar <z5594044@ad.unsw.edu.au>
2026-05-28 18:40:08 +1000

    added changing color for pos and neg realised and unrealised profits

--- app_logic/current_profit_render.js
+++ app_logic/current_profit_render.js
@@ -1,14 +1,22 @@
+import { blackColor, closeGreyColor, greenColor, sellRedColor } from "./constants.js"
 import { TRADE_CLOSED } from "./trade_execution.js"
 
 export class RealisedProfitRender {
     constructor() {
         this.currentBalanceText = document.querySelector('.main-loop .right-panel .realised-profit .amount-text')
-        this.currentBalanceText.textContent = `$${0}`
+        this.currentBalanceText.textContent = `$0.00`
     }
 
     updateBalance(currTradeInfo) {
         if (currTradeInfo.type === TRADE_CLOSED) {
             this.currentBalanceText.textContent = `$${currTradeInfo.realisedProfit.toFixed(2)}`
+            if (currTradeInfo.realisedProfit > 0) {
+                this.currentBalanceText.style.color = greenColor
+            } else if (currTradeInfo.realisedProfit < 0) {
+                this.currentBalanceText.style.color = sellRedColor
+            } else {
+                this.currentBalanceText.style.color = blackColor
+            }
         }
     }
 }
@@ -16,10 +24,18 @@ export class RealisedProfitRender {
 export class UnrealisedProfitRender {
     constructor() {
         this.currentBalanceText = document.querySelector('.main-loop .right-panel .unrealised-profit .amount-text')
-        this.currentBalanceText.textContent = `$${0}`
+        this.currentBalanceText.textContent = `$0.00`
     }
 
     updateBalance(currUpdate) {
         this.currentBalanceText.textContent = `$${currUpdate.unrealisedProfit.toFixed(2)}`
+
+        if (currUpdate.unrealisedProfit > 0) {
+            this.currentBalanceText.style.color = greenColor
+        } else if (currUpdate.unrealisedProfit < 0) {
+            this.currentBalanceText.style.color = sellRedColor
+        } else {
+            this.currentBalanceText.style.color = blackColor
+        }
     }
 }

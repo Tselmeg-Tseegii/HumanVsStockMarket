93bdcf3fc5720500e10d70fcb9c0d95586fe0eaa
Tselmeg Otgonbayar <z5594044@ad.unsw.edu.au>
2026-05-28 19:30:54 +1000

    trade history is now properly organised into three columns with color coded outcomes

--- app_logic/trade_history.js
+++ app_logic/trade_history.js
@@ -1,24 +1,43 @@
+import { greenColor, sellRedColor } from "./constants.js"
 import { 
     TRADE_EXE_BOUGHT
 } from "./trade_execution.js"
 
 export class TradeHistoryRender {
     constructor() {
-        this.tradeHistory = []
         this.list = document.querySelector('.main-loop .right-panel .trade-history .trade-history-content .list')
+        this.numTrades = 0
     }
 
     updateHistory(tradeHistoryItem) {
+        this.numTrades++
         const newItem = document.createElement('li')
 
-        let tradeType = ''
+        const numCol = document.createElement('div')
+        numCol.classList.add('col-num')
+        numCol.textContent = `${this.numTrades}`
+
+        const typeCol = document.createElement('div')
+        typeCol.classList.add('col-type')
         if (tradeHistoryItem.type === TRADE_EXE_BOUGHT) {
-            tradeType = 'Buy'
+            typeCol.textContent = 'Buy'
         } else {
-            tradeType = 'Sell'
+            typeCol.textContent = 'Sell'
         }
 
-        newItem.textContent = `${tradeType} - Outcome: $${tradeHistoryItem.outcome.toFixed(2)}`
+        const outcomeCol = document.createElement('div')
+        outcomeCol.classList.add('col-outcome')
+        outcomeCol.textContent = `$${tradeHistoryItem.outcome.toFixed(2)}`
+        if (tradeHistoryItem.outcome > 0) {
+            outcomeCol.style.color = greenColor
+        } else if (tradeHistoryItem.outcome < 0) {
+            outcomeCol.style.color = sellRedColor
+        }
+
+        newItem.appendChild(numCol)
+        newItem.appendChild(typeCol)
+        newItem.appendChild(outcomeCol)
+
         this.list.prepend(newItem)
         this.list.scrollTop = 0
     }

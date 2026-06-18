16f2cff7c45abc27a48cfb5529a69f8c5b17ab35
Tselmeg Otgonbayar <tselmeg.rim@gmail.com>
2026-06-19 08:39:13 +1000

    forgot to export the schema

--- data_schema.js
+++ data_schema.js
@@ -42,7 +42,7 @@ export const saveDataPayloadSchema = z.object({
     tradeHistoryData: tradeHistoryDataSchema
 })
 
-const profitParamSchema = z.string().regex(/^-?\d+(\.\d+)?$/, "Must be a valid number")
+export const profitParamSchema = z.string().regex(/^-?\d+(\.\d+)?$/, "Must be a valid number")
 
 export const histogramBinSchema = z.object({
     binIndex: z.coerce.number().int(), 
